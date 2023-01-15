10adeac91d393d2557570884b146a8d260c8af60
MeriemAitOuaziz <meriem.aitouaziz@gmail.com>
2023-01-15 18:47:28 +0100

    backend

--- agrodig_backend/post-service/src/main/java/com/agrodig/postservice/PostServiceApplication.java
+++ agrodig_backend/post-service/src/main/java/com/agrodig/postservice/PostServiceApplication.java
@@ -2,8 +2,9 @@ package com.agrodig.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
