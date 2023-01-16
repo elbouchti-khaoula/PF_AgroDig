27ba247a23d51e3874ee66f1e7ccb812ea85c87e
MeriemAitOuaziz <meriem.aitouaziz@gmail.com>
2023-01-16 18:33:58 +0100

    fixed discovery service bug

--- agrodig_backend/users-service/src/main/java/com/agrodig/usersservice/UsersServiceApplication.java
+++ agrodig_backend/users-service/src/main/java/com/agrodig/usersservice/UsersServiceApplication.java
@@ -5,14 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaClient
+@ComponentScan("com.agrodig.usersservice.repository")
 public class UsersServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UsersServiceApplication.class, args);
-
     }
+
 }
