c66ebb28a397c140246673f6be0e3a1e79e0d7de
MeriemAitOuaziz <meriem.aitouaziz@gmail.com>
2023-01-16 22:36:45 +0100

    added data src

--- agrodig_backend/blog-service/src/main/java/com/agrodig/blogservice/BlogServiceApplication.java
+++ agrodig_backend/blog-service/src/main/java/com/agrodig/blogservice/BlogServiceApplication.java
@@ -1,4 +1,4 @@
-package com.agrodig;
+package com.agrodig.blogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- agrodig_backend/users-service/src/main/java/com/agrodig/usersservice/UsersServiceApplication.java
+++ agrodig_backend/users-service/src/main/java/com/agrodig/usersservice/UsersServiceApplication.java
@@ -7,9 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableEurekaClient
-@ComponentScan("com.agrodig.usersservice.repository")
 public class UsersServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UsersServiceApplication.class, args);
