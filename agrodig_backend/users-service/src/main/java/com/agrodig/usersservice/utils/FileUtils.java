9a14305d958001d0706988ac088b26aee427f7c2
ChaimaaNajim <nmchaimaa9@gmail.com>
2023-01-18 00:32:12 +0100

    trying to add upload pic

---
+++ agrodig_backend/users-service/src/main/java/com/agrodig/usersservice/utils/FileUtils.java
@@ -0,0 +1,23 @@
+package com.agrodig.usersservice.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileUtils {
+    public static void saveFile(MultipartFile file, String directory, String fileName) {
+        if (!file.isEmpty()) {
+            try {
+                if (!new File(directory).exists()) {
+                    new File(directory).mkdirs();
+                }
+                String filePath = directory + fileName;
+                File dest = new File(filePath);
+                file.transferTo(dest);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
